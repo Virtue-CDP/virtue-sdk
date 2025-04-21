ec1ae072b8b48791f44815b9e8dec5e3ac3d9b37
moly369 <jmolly369@outlook.com>
2025-04-21 22:26:32 +0900

    feat: add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,196 @@
+import { Transaction, TransactionResult, TransactionArgument } from '@iota/iota-sdk/transactions';
+import { IotaClient, IotaObjectData, IotaParsedData, IotaObjectResponse, IotaMoveObject } from '@iota/iota-sdk/client';
+import * as superstruct from 'superstruct';
+import { Infer } from 'superstruct';
+import { SharedObjectRef } from '@iota/iota-sdk/dist/cjs/bcs/types';
+
+type COIN = "IOTA" | "stIOTA" | "VUSD";
+type COLLATERAL_COIN = "IOTA";
+
+type Float = {
+    fields: {
+        value: string;
+    };
+};
+type VaultResponse = {
+    balance: string;
+    decimal: number;
+    min_debt_amount: string;
+    limited_supply: {
+        fields: {
+            limit: string;
+            supply: string;
+        };
+    };
+    liquidation_config: {
+        fields: {
+            mcr: Float;
+            ccr: Float;
+            bcr: Float;
+        };
+    };
+    position_table: {
+        fields: {
+            fee_rate: Float;
+            interest_rate: Float;
+            timestamp: number;
+            table: {
+                fields: {
+                    head: string | null;
+                    tail: string | null;
+                    id: {
+                        id: string;
+                    };
+                    size: string;
+                };
+            };
+        };
+    };
+    surplus_table: {
+        fields: {
+            id: {
+                id: string;
+            };
+            size: string;
+        };
+    };
+};
+
+type VaultInfo = {
+    token: COLLATERAL_COIN;
+    baseFeeRate: number;
+    bottleTableSize: string;
+    bottleTableId: string;
+    collateralDecimal: number;
+    collateralVault: string;
+    latestRedemptionTime: number;
+    minCollateralRatio: string;
+    mintedBuckAmount: string;
+    minBottleSize: string;
+    maxMintAmount: string;
+    recoveryModeThreshold: string;
+};
+type Position = {
+    collAmount: string;
+    debtAmount: string;
+};
+type VaultInfoList = Partial<Record<COLLATERAL_COIN, VaultInfo>>;
+
+declare class VirtueClient {
+    network: string;
+    owner: string;
+    /**
+     * @description a TS wrapper over Virtue CDP client.
+     * @param network connection to fullnode: 'mainnet' | 'testnet' | 'devnet' | 'localnet' | string
+     * @param owner (optional) address of the current user (default: DUMMY_ADDRESS)
+     */
+    private rpcEndpoint;
+    private client;
+    constructor(network?: string, owner?: string);
+    getClient(): IotaClient;
+    /**
+     * @description Get Vault<token> object
+     */
+    getVaultInfo(coinSymbol: COLLATERAL_COIN): Promise<VaultInfo>;
+    getPosition(debtor: string, coinSymbol: COLLATERAL_COIN): Promise<Position | undefined>;
+    /**
+     * @description Create a price collector
+     * @param collateral coin symbol, e.g "IOTA"
+     */
+    newPriceCollector(tx: Transaction, coinSymbol: COLLATERAL_COIN): TransactionResult;
+    /**
+     * @description Get a price result
+     * @param collateral coin symbol, e.g "IOTA"
+     */
+    aggregatePrice(tx: Transaction, coinSymbol: COLLATERAL_COIN): TransactionResult;
+    /**
+     * @description Get a request to Mange Position
+     * @param tx
+     * @param collateral coin symbol , e.g "IOTA"
+     * @param collateral input coin
+     * @param the amount to borrow
+     * @param repyment input coin (always VUSD)
+     * @param the amount to withdraw
+     * @returns ManageRequest
+     */
+    requestManagePosition(tx: Transaction, coinSymbol: COLLATERAL_COIN, depositCoin: TransactionArgument, borrowAmount: string, repaymentCoin: TransactionArgument, withdrawAmount: string, accountObj?: string | TransactionArgument): TransactionResult;
+    /**
+     * @description Manage Position
+     * @param tx
+     * @param collateral coin symbol , e.g "IOTA"
+     * @param manager request, see this.requestManagePosition
+     * @param price result, see this.getPriceResult
+     * @param the position place to insert
+     * @returns [Coin<T>, COIN<VUSD>]
+     */
+    managePosition(tx: Transaction, coinSymbol: COLLATERAL_COIN, manageRequest: TransactionArgument, priceResult?: TransactionArgument, insertionPlace?: string): TransactionResult;
+}
+
+declare function getObjectNames(objectTypes: string[]): string[];
+declare const getCoinType: (str: string) => string | null;
+declare const getCoinTypeFromTank: (tankType: string) => string | null;
+declare const getCoinTypeFromPipe: (pipeType: string) => string | null;
+declare const getCoinSymbol: (coinType: string) => COIN | undefined;
+declare function U64FromBytes(x: number[]): bigint;
+declare const formatUnits: (value: bigint, decimals: number) => string;
+declare const parseUnits: (value: number | string, decimals: number) => bigint;
+declare const getPriceResultType: (coinSymbol: COLLATERAL_COIN) => string;
+
+declare function coinIntoBalance(tx: Transaction, coinType: string, coinInput: TransactionArgument | undefined): TransactionResult;
+declare function coinFromBalance(tx: Transaction, coinType: string, balanceInput: TransactionArgument): TransactionResult;
+declare function getInputCoins(tx: Transaction, client: IotaClient, owner: string, coinType: string, ...amounts: string[]): Promise<TransactionResult>;
+declare function getMainCoin(tx: Transaction, client: IotaClient, owner: string, coinType: string): Promise<TransactionResult | {
+    $kind: "Input";
+    Input: number;
+    type?: "object";
+} | undefined>;
+
+declare const ObjectContentFields: superstruct.Struct<Record<string, any>, null>;
+type ObjectContentFields = Infer<typeof ObjectContentFields>;
+interface IotaObjectDataWithContent extends IotaObjectData {
+    content: IotaParsedData;
+}
+declare function getIotaObjectData(resp: IotaObjectResponse): IotaObjectData | null | undefined;
+declare function getMoveObject(data: IotaObjectResponse | IotaObjectData): IotaMoveObject | undefined;
+declare function getObjectFields(resp: IotaObjectResponse | IotaMoveObject | IotaObjectData): ObjectContentFields | undefined;
+declare const getObjectGenerics: (resp: IotaObjectResponse) => string[];
+
+declare const COINS_TYPE_LIST: Record<COIN, string>;
+declare const COIN_DECIMALS: Record<COIN, number>;
+
+declare const ORIGINAL_FRAMEWORK_PACKAGE_ID = "0x6f8dd0377fe5469cd3456350ca13ae1799655fda06e90191b73ab1c0c0165e8f";
+declare const ORIGINAL_VUSD_PACKAGE_ID = "0x929065320c756b8a4a841deeed013bd748ee45a28629c4aaafc56d8948ebb081";
+declare const ORIGINAL_ORACLE_PACKAGE_ID = "0xbc672e6330ab22078715f86e952ef1353d9f9b217c4579e47ce29eaec6f92655";
+declare const ORIGINAL_CDP_PACKAGE_ID = "0x0731a9f5cbdb0a4aea3f540280a1a266502017867734240e29edc813074e7f60";
+declare const FRAMEWORK_PACKAGE_ID = "0x6f8dd0377fe5469cd3456350ca13ae1799655fda06e90191b73ab1c0c0165e8f";
+declare const VUSD_PACKAGE_ID = "0x929065320c756b8a4a841deeed013bd748ee45a28629c4aaafc56d8948ebb081";
+declare const ORACLE_PACKAGE_ID = "0xbc672e6330ab22078715f86e952ef1353d9f9b217c4579e47ce29eaec6f92655";
+declare const CDP_PACKAGE_ID = "0x0731a9f5cbdb0a4aea3f540280a1a266502017867734240e29edc813074e7f60";
+declare const CLOCK_OBJ: {
+    objectId: string;
+    mutable: boolean;
+    initialSharedVersion: number;
+};
+declare const TREASURY_OBJ: {
+    objectId: string;
+    mutable: boolean;
+    initialSharedVersion: number;
+};
+declare const CDP_VERSION_OBJ: {
+    objectId: string;
+    mutable: boolean;
+    initialSharedVersion: number;
+};
+type VaultObjectInfo = {
+    priceAggregater: SharedObjectRef;
+    vault: SharedObjectRef;
+};
+declare const VAULT_MAP: Record<COLLATERAL_COIN, VaultObjectInfo>;
+declare const TESTNET_PRICE_PACKAGE_ID = "0x2de2d918f5940978dc53aae2ea0687a4ca8a6736bd525f15ee17e9529048fa92";
+declare const TESTNET_PRICE_FEED_OBJ: {
+    objectId: string;
+    mutable: boolean;
+    initialSharedVersion: number;
+};
+
+export { CDP_PACKAGE_ID, CDP_VERSION_OBJ, CLOCK_OBJ, type COIN, COINS_TYPE_LIST, COIN_DECIMALS, type COLLATERAL_COIN, FRAMEWORK_PACKAGE_ID, type Float, type IotaObjectDataWithContent, ORACLE_PACKAGE_ID, ORIGINAL_CDP_PACKAGE_ID, ORIGINAL_FRAMEWORK_PACKAGE_ID, ORIGINAL_ORACLE_PACKAGE_ID, ORIGINAL_VUSD_PACKAGE_ID, ObjectContentFields, type Position, TESTNET_PRICE_FEED_OBJ, TESTNET_PRICE_PACKAGE_ID, TREASURY_OBJ, U64FromBytes, VAULT_MAP, VUSD_PACKAGE_ID, type VaultInfo, type VaultInfoList, type VaultObjectInfo, type VaultResponse, VirtueClient, coinFromBalance, coinIntoBalance, formatUnits, getCoinSymbol, getCoinType, getCoinTypeFromPipe, getCoinTypeFromTank, getInputCoins, getIotaObjectData, getMainCoin, getMoveObject, getObjectFields, getObjectGenerics, getObjectNames, getPriceResultType, parseUnits };
---
+++ dist/index.js
@@ -0,0 +1,463 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _nullishCoalesce(lhs, rhsFn) { if (lhs != null) { return lhs; } else { return rhsFn(); } } function _optionalChain(ops) { let lastAccessLHS = undefined; let value = ops[0]; let i = 1; while (i < ops.length) { const op = ops[i]; const fn = ops[i + 1]; i += 2; if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) { return undefined; } if (op === 'access' || op === 'optionalAccess') { lastAccessLHS = value; value = fn(value); } else if (op === 'call' || op === 'optionalCall') { value = fn((...args) => value.call(lastAccessLHS, ...args)); lastAccessLHS = undefined; } } return value; }// src/client.ts
+var _client = require('@iota/iota-sdk/client');
+
+// src/constants/coin.ts
+var COINS_TYPE_LIST = {
+  IOTA: "0x0000000000000000000000000000000000000000000000000002::iota::iota",
+  stIOTA: "0x14f9e69c0076955d5a056260c9667edab184650dba9919f168a37030dd956dc6::cert::cert",
+  VUSD: "0x929065320c756b8a4a841deeed013bd748ee45a28629c4aaafc56d8948ebb081::vusd::vusd"
+};
+var COIN_DECIMALS = {
+  IOTA: 9,
+  stIOTA: 9,
+  VUSD: 6
+};
+
+// src/constants/object.ts
+var ORIGINAL_FRAMEWORK_PACKAGE_ID = "0x6f8dd0377fe5469cd3456350ca13ae1799655fda06e90191b73ab1c0c0165e8f";
+var ORIGINAL_VUSD_PACKAGE_ID = "0x929065320c756b8a4a841deeed013bd748ee45a28629c4aaafc56d8948ebb081";
+var ORIGINAL_ORACLE_PACKAGE_ID = "0xbc672e6330ab22078715f86e952ef1353d9f9b217c4579e47ce29eaec6f92655";
+var ORIGINAL_CDP_PACKAGE_ID = "0x0731a9f5cbdb0a4aea3f540280a1a266502017867734240e29edc813074e7f60";
+var FRAMEWORK_PACKAGE_ID = "0x6f8dd0377fe5469cd3456350ca13ae1799655fda06e90191b73ab1c0c0165e8f";
+var VUSD_PACKAGE_ID = "0x929065320c756b8a4a841deeed013bd748ee45a28629c4aaafc56d8948ebb081";
+var ORACLE_PACKAGE_ID = "0xbc672e6330ab22078715f86e952ef1353d9f9b217c4579e47ce29eaec6f92655";
+var CDP_PACKAGE_ID = "0x0731a9f5cbdb0a4aea3f540280a1a266502017867734240e29edc813074e7f60";
+var CLOCK_OBJ = {
+  objectId: "0x0000000000000000000000000000000000000000000000000000000000000006",
+  mutable: false,
+  initialSharedVersion: 1
+};
+var TREASURY_OBJ = {
+  objectId: "0x4e2e41f0158bce26c8e8ccc62be3ab5326d8b294a2bcccfbe0e7298885f66eb7",
+  mutable: true,
+  initialSharedVersion: 190869396
+};
+var CDP_VERSION_OBJ = {
+  objectId: "0xb67e79921b2b71e77e5bd1adf28c9d47a8ad5bd22e024bbd90797514c39b068d",
+  mutable: false,
+  initialSharedVersion: 237544284
+};
+var VAULT_MAP = {
+  IOTA: {
+    priceAggregater: {
+      objectId: "0x9f3c9d72993efd5e2e543ad69bdc0bbe1bd9873c3a61fdf32c0cf48660a5f2c8",
+      mutable: false,
+      initialSharedVersion: 190869399
+    },
+    vault: {
+      objectId: "0xbbf7b7667aca64405e8756f6974f41bd648bd1bc40dfc2b1cfe2d6ec419eedb1",
+      mutable: true,
+      initialSharedVersion: 237544284
+    }
+  }
+};
+var TESTNET_PRICE_PACKAGE_ID = "0x2de2d918f5940978dc53aae2ea0687a4ca8a6736bd525f15ee17e9529048fa92";
+var TESTNET_PRICE_FEED_OBJ = {
+  objectId: "0x05cc35b8d331a3893f80b9ca6c70c3b75298e9cbf1b5d707b6d18c40b0b3da5d",
+  mutable: false,
+  initialSharedVersion: 190869400
+};
+
+// src/utils/format.ts
+function getObjectNames(objectTypes) {
+  const accept_coin_type = Object.values(COINS_TYPE_LIST);
+  const accept_coin_name = Object.keys(COINS_TYPE_LIST);
+  const coinTypeList = objectTypes.map(
+    (type) => _nullishCoalesce(_optionalChain([type, 'access', _ => _.split, 'call', _2 => _2("<"), 'access', _3 => _3.pop, 'call', _4 => _4(), 'optionalAccess', _5 => _5.replace, 'call', _6 => _6(">", "")]), () => ( ""))
+  );
+  const objectNameList = [];
+  coinTypeList.forEach((type) => {
+    const typeIndex = accept_coin_type.indexOf(type);
+    const coinName = accept_coin_name[typeIndex];
+    objectNameList.push(_nullishCoalesce(coinName, () => ( "")));
+  });
+  return objectNameList;
+}
+var getCoinType = (str) => {
+  const startIndex = str.indexOf("<");
+  const endIndex = str.lastIndexOf(">");
+  if (startIndex !== -1 && endIndex !== -1 && endIndex > startIndex) {
+    return str.slice(startIndex + 1, endIndex);
+  }
+  return null;
+};
+var getCoinTypeFromTank = (tankType) => {
+  const tankGroup = tankType.split("::tank::Tank");
+  if (tankGroup.length < 2) return null;
+  const coinGroup = (_nullishCoalesce(getCoinType(tankGroup[1]), () => ( ""))).split(", ");
+  if (coinGroup.length < 2) return null;
+  const coinType = coinGroup[1].trim();
+  return coinType;
+};
+var getCoinTypeFromPipe = (pipeType) => {
+  const pipeGroup = pipeType.split("::pipe::Pipe");
+  if (pipeGroup.length < 2) return null;
+  const coinGroup = (_nullishCoalesce(getCoinType(pipeGroup[1]), () => ( ""))).split(", ");
+  if (coinGroup.length < 2) return null;
+  const coinType = coinGroup[0].trim();
+  return coinType;
+};
+var getCoinSymbol = (coinType) => {
+  const coin = Object.keys(COINS_TYPE_LIST).find(
+    (key) => COINS_TYPE_LIST[key] === coinType
+  );
+  if (coin) {
+    return coin;
+  }
+  return void 0;
+};
+function U64FromBytes(x) {
+  let u64 = BigInt(0);
+  for (let i = x.length - 1; i >= 0; i--) {
+    u64 = u64 << BigInt(8) | BigInt(_nullishCoalesce(x[i], () => ( 0)));
+  }
+  return u64;
+}
+var formatUnits = (value, decimals) => {
+  let display = value.toString();
+  const negative = display.startsWith("-");
+  if (negative) display = display.slice(1);
+  display = display.padStart(decimals, "0");
+  const integer = display.slice(0, display.length - decimals);
+  let fraction = display.slice(display.length - decimals);
+  fraction = fraction.replace(/(0+)$/, "");
+  return `${negative ? "-" : ""}${integer || "0"}${fraction ? `.${fraction}` : ""}`;
+};
+var parseUnits = (value, decimals) => {
+  let [integer, fraction = "0"] = typeof value == "string" ? value.split(".") : value.toString().split(".");
+  if (integer === void 0) {
+    return BigInt(0);
+  }
+  const negative = integer.startsWith("-");
+  if (negative) integer = integer.slice(1);
+  fraction = fraction.replace(/(0+)$/, "");
+  if (decimals === 0) {
+    integer = `${Math.round(Number(`${integer}.${fraction}`))}`;
+    fraction = "";
+  } else if (fraction.length > decimals) {
+    const [before, after] = [
+      fraction.slice(0, decimals),
+      fraction.slice(decimals)
+    ];
+    fraction = `${/^0+$/.test(before) ? before.slice(0, before.length - 1) : ""}${Math.round(Number(`${before}.${after}`))}`;
+  } else {
+    fraction = fraction.padEnd(decimals, "0");
+  }
+  return BigInt(`${negative ? "-" : ""}${integer}${fraction}`);
+};
+var getPriceResultType = (coinSymbol) => {
+  return `${ORIGINAL_ORACLE_PACKAGE_ID}::result::PriceResult<${COINS_TYPE_LIST[coinSymbol]}>`;
+};
+
+// src/utils/coin.ts
+function coinIntoBalance(tx, coinType, coinInput) {
+  if (coinInput) {
+    return tx.moveCall({
+      target: "0x2::coin::into_balance",
+      typeArguments: [coinType],
+      arguments: [coinInput]
+    });
+  } else {
+    return tx.moveCall({
+      target: "0x2::balance::zero",
+      typeArguments: [coinType]
+    });
+  }
+}
+function coinFromBalance(tx, coinType, balanceInput) {
+  return tx.moveCall({
+    target: "0x2::coin::from_balance",
+    typeArguments: [coinType],
+    arguments: [balanceInput]
+  });
+}
+async function getInputCoins(tx, client, owner, coinType, ...amounts) {
+  let isZero = true;
+  for (const amount of amounts) {
+    if (Number(amount) > 0) {
+      isZero = false;
+      break;
+    }
+  }
+  if (isZero) {
+    return tx.moveCall({
+      target: `0x2::coin::zero`,
+      typeArguments: [coinType]
+    });
+  }
+  if (coinType === COINS_TYPE_LIST.IOTA) {
+    return tx.splitCoins(
+      tx.gas,
+      amounts.map((amount) => tx.pure.u64(amount))
+    );
+  } else {
+    const { data: userCoins } = await client.getCoins({ owner, coinType });
+    const [mainCoin, ...otherCoins] = userCoins.map(
+      (coin) => tx.objectRef({
+        objectId: coin.coinObjectId,
+        version: coin.version,
+        digest: coin.digest
+      })
+    );
+    if (!mainCoin) {
+      return tx.moveCall({
+        target: `0x2::coin::zero`,
+        typeArguments: [coinType]
+      });
+    }
+    if (otherCoins.length > 0) tx.mergeCoins(mainCoin, otherCoins);
+    return tx.splitCoins(
+      mainCoin,
+      amounts.map((amount) => tx.pure.u64(amount))
+    );
+  }
+}
+async function getMainCoin(tx, client, owner, coinType) {
+  if (coinType === COINS_TYPE_LIST.IOTA) {
+    return void 0;
+  }
+  const { data: userCoins } = await client.getCoins({ owner, coinType });
+  const [mainCoin, ...otherCoins] = userCoins.map(
+    (coin) => tx.objectRef({
+      objectId: coin.coinObjectId,
+      version: coin.version,
+      digest: coin.digest
+    })
+  );
+  if (!mainCoin) {
+    return tx.moveCall({
+      target: `0x2::coin::zero`,
+      typeArguments: [coinType]
+    });
+  }
+  if (otherCoins.length > 0) tx.mergeCoins(mainCoin, otherCoins);
+  return mainCoin;
+}
+
+// src/utils/object.ts
+var _superstruct = require('superstruct');
+var ObjectContentFields = _superstruct.record.call(void 0, _superstruct.string.call(void 0, ), _superstruct.any.call(void 0, ));
+function isIotaObjectDataWithContent(data) {
+  return data.content !== void 0;
+}
+function getIotaObjectData(resp) {
+  return resp.data;
+}
+function getMoveObject(data) {
+  const obj = "data" in data ? getIotaObjectData(data) : data;
+  if (!obj || !isIotaObjectDataWithContent(obj) || obj.content.dataType !== "moveObject") {
+    return void 0;
+  }
+  return obj.content;
+}
+function getObjectFields(resp) {
+  if ("fields" in resp) {
+    return resp.fields;
+  }
+  return _optionalChain([getMoveObject, 'call', _7 => _7(resp), 'optionalAccess', _8 => _8.fields]);
+}
+var getObjectGenerics = (resp) => {
+  const objType = _optionalChain([resp, 'access', _9 => _9.data, 'optionalAccess', _10 => _10.type]);
+  const startIdx = _optionalChain([objType, 'optionalAccess', _11 => _11.indexOf, 'optionalCall', _12 => _12("<")]);
+  const endIdx = _optionalChain([objType, 'optionalAccess', _13 => _13.lastIndexOf, 'optionalCall', _14 => _14(">")]);
+  return startIdx ? objType.slice(startIdx + 1, endIdx).split(", ") : [];
+};
+
+// src/client.ts
+var DUMMY_ADDRESS = "0x0";
+var VirtueClient = class {
+  constructor(network = "mainnet", owner = DUMMY_ADDRESS) {
+    this.network = network;
+    this.owner = owner;
+    if (network == "mainnet" || network == "testnet" || network == "devnet" || network == "localnet") {
+      this.rpcEndpoint = _client.getFullnodeUrl.call(void 0, network);
+    } else {
+      this.rpcEndpoint = network;
+    }
+    this.client = new (0, _client.IotaClient)({ url: this.rpcEndpoint });
+  }
+  getClient() {
+    return this.client;
+  }
+  // Query
+  /**
+   * @description Get Vault<token> object
+   */
+  async getVaultInfo(coinSymbol) {
+    const vault = VAULT_MAP[coinSymbol];
+    const res = await this.client.getObject({
+      id: vault.vault.objectId,
+      options: {
+        showContent: true
+      }
+    });
+    const fields = getObjectFields(res);
+    const vaultInfo = {
+      token: coinSymbol,
+      baseFeeRate: Number(_nullishCoalesce(fields.position_table.fields.fee_rate, () => ( 3e6))) / 10 ** 9,
+      bottleTableSize: fields.position_table.fields.table.fields.size,
+      bottleTableId: fields.position_table.fields.table.fields.id.id,
+      collateralDecimal: Number(fields.decimal),
+      collateralVault: fields.balance,
+      latestRedemptionTime: Number(fields.position_table.fields.timestamp),
+      minCollateralRatio: fields.liquidation_config.fields.mcr.fields.value,
+      mintedBuckAmount: fields.limited_supply.fields.supply,
+      maxMintAmount: fields.limited_supply.fields.limit,
+      recoveryModeThreshold: fields.liquidation_config.fields.ccr.fields.value,
+      minBottleSize: fields.min_debt_amount
+    };
+    return vaultInfo;
+  }
+  async getPosition(debtor, coinSymbol) {
+    const vaultInfo = await this.getVaultInfo(coinSymbol);
+    const tableId = vaultInfo.bottleTableId;
+    const positionRes = await this.client.getDynamicFieldObject({
+      parentId: tableId,
+      name: {
+        type: "address",
+        value: debtor
+      }
+    });
+    const positionData = getObjectFields(positionRes);
+    if (!positionData) return;
+    positionData;
+    return {
+      collAmount: positionData.coll_amount,
+      debtAmount: positionData.debt_amount
+    };
+  }
+  /**
+   * @description Create a price collector
+   * @param collateral coin symbol, e.g "IOTA"
+   */
+  newPriceCollector(tx, coinSymbol) {
+    return tx.moveCall({
+      target: `${ORACLE_PACKAGE_ID}::collector::new`,
+      typeArguments: [COINS_TYPE_LIST[coinSymbol]]
+    });
+  }
+  /**
+   * @description Get a price result
+   * @param collateral coin symbol, e.g "IOTA"
+   */
+  aggregatePrice(tx, coinSymbol) {
+    const [collector] = this.newPriceCollector(tx, coinSymbol);
+    const coinType = COINS_TYPE_LIST[coinSymbol];
+    tx.moveCall({
+      target: `${TESTNET_PRICE_PACKAGE_ID}::testnet_price::feed_price`,
+      typeArguments: [coinType],
+      arguments: [tx.sharedObjectRef(TESTNET_PRICE_FEED_OBJ), collector]
+    });
+    const aggregater = tx.sharedObjectRef(
+      VAULT_MAP[coinSymbol].priceAggregater
+    );
+    return tx.moveCall({
+      target: `${ORACLE_PACKAGE_ID}::aggregater::aggregate`,
+      typeArguments: [coinType],
+      arguments: [aggregater, collector]
+    });
+  }
+  /**
+   * @description Get a request to Mange Position
+   * @param tx
+   * @param collateral coin symbol , e.g "IOTA"
+   * @param collateral input coin
+   * @param the amount to borrow
+   * @param repyment input coin (always VUSD)
+   * @param the amount to withdraw
+   * @returns ManageRequest
+   */
+  requestManagePosition(tx, coinSymbol, depositCoin, borrowAmount, repaymentCoin, withdrawAmount, accountObj) {
+    const coinType = COINS_TYPE_LIST[coinSymbol];
+    const [accountReq] = accountObj ? tx.moveCall({
+      target: `${FRAMEWORK_PACKAGE_ID}::account::request_with_account`,
+      arguments: [
+        typeof accountObj === "string" ? tx.object(accountObj) : accountObj
+      ]
+    }) : tx.moveCall({
+      target: `${FRAMEWORK_PACKAGE_ID}::account::request`
+    });
+    return tx.moveCall({
+      target: `${CDP_PACKAGE_ID}::manager::request`,
+      typeArguments: [coinType],
+      arguments: [
+        tx.sharedObjectRef(CDP_VERSION_OBJ),
+        accountReq,
+        depositCoin,
+        tx.pure.u64(borrowAmount),
+        repaymentCoin,
+        tx.pure.u64(withdrawAmount)
+      ]
+    });
+  }
+  /**
+   * @description Manage Position
+   * @param tx
+   * @param collateral coin symbol , e.g "IOTA"
+   * @param manager request, see this.requestManagePosition
+   * @param price result, see this.getPriceResult
+   * @param the position place to insert
+   * @returns [Coin<T>, COIN<VUSD>]
+   */
+  managePosition(tx, coinSymbol, manageRequest, priceResult, insertionPlace) {
+    const vault = VAULT_MAP[coinSymbol].vault;
+    const priceResultOpt = priceResult ? tx.moveCall({
+      target: `0x2::option::some`,
+      typeArguments: [getPriceResultType(coinSymbol)]
+    }) : tx.moveCall({
+      target: `0x2::option::none`,
+      typeArguments: [getPriceResultType(coinSymbol)]
+    });
+    return tx.moveCall({
+      target: `${CDP_PACKAGE_ID}::vault::manage_position`,
+      typeArguments: [COINS_TYPE_LIST[coinSymbol]],
+      arguments: [
+        tx.sharedObjectRef(vault),
+        tx.sharedObjectRef(CDP_VERSION_OBJ),
+        tx.sharedObjectRef(TREASURY_OBJ),
+        tx.sharedObjectRef(CLOCK_OBJ),
+        priceResultOpt,
+        manageRequest,
+        tx.pure.option("address", insertionPlace)
+      ]
+    });
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.CDP_PACKAGE_ID = CDP_PACKAGE_ID; exports.CDP_VERSION_OBJ = CDP_VERSION_OBJ; exports.CLOCK_OBJ = CLOCK_OBJ; exports.COINS_TYPE_LIST = COINS_TYPE_LIST; exports.COIN_DECIMALS = COIN_DECIMALS; exports.FRAMEWORK_PACKAGE_ID = FRAMEWORK_PACKAGE_ID; exports.ORACLE_PACKAGE_ID = ORACLE_PACKAGE_ID; exports.ORIGINAL_CDP_PACKAGE_ID = ORIGINAL_CDP_PACKAGE_ID; exports.ORIGINAL_FRAMEWORK_PACKAGE_ID = ORIGINAL_FRAMEWORK_PACKAGE_ID; exports.ORIGINAL_ORACLE_PACKAGE_ID = ORIGINAL_ORACLE_PACKAGE_ID; exports.ORIGINAL_VUSD_PACKAGE_ID = ORIGINAL_VUSD_PACKAGE_ID; exports.ObjectContentFields = ObjectContentFields; exports.TESTNET_PRICE_FEED_OBJ = TESTNET_PRICE_FEED_OBJ; exports.TESTNET_PRICE_PACKAGE_ID = TESTNET_PRICE_PACKAGE_ID; exports.TREASURY_OBJ = TREASURY_OBJ; exports.U64FromBytes = U64FromBytes; exports.VAULT_MAP = VAULT_MAP; exports.VUSD_PACKAGE_ID = VUSD_PACKAGE_ID; exports.VirtueClient = VirtueClient; exports.coinFromBalance = coinFromBalance; exports.coinIntoBalance = coinIntoBalance; exports.formatUnits = formatUnits; exports.getCoinSymbol = getCoinSymbol; exports.getCoinType = getCoinType; exports.getCoinTypeFromPipe = getCoinTypeFromPipe; exports.getCoinTypeFromTank = getCoinTypeFromTank; exports.getInputCoins = getInputCoins; exports.getIotaObjectData = getIotaObjectData; exports.getMainCoin = getMainCoin; exports.getMoveObject = getMoveObject; exports.getObjectFields = getObjectFields; exports.getObjectGenerics = getObjectGenerics; exports.getObjectNames = getObjectNames; exports.getPriceResultType = getPriceResultType; exports.parseUnits = parseUnits;
+//# sourceMappingURL=index.js.map
